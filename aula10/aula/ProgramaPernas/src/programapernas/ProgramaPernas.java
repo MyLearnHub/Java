83c74f9fa70459f36f28b1ff02cc48b5cc94fdeb
Emy-Liberato <paulo.alvares@sptech.school>
2024-08-07 03:38:27 -0300

    feat: nested conditional structures in Java

---
+++ aula10/aula/ProgramaPernas/src/programapernas/ProgramaPernas.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package programapernas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pauloalvares
+ */
+public class ProgramaPernas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner tec = new Scanner(System.in);
+        
+        System.out.println("Quantas pernas? ");
+        int perna = tec.nextInt();
+        String tipo = "";
+        
+        switch (perna) {
+            case 1:
+                tipo = "Saci";
+                break;
+            case 2:
+                tipo = "Bípede";
+                break;
+            case 3:
+                tipo = "Tripé";
+                break;
+            case 4:
+                tipo = "Quadrúpede";
+                break;
+            case 6:
+                tipo = "Aranha";
+                break;
+            default:
+                tipo = "ET";
+                break;
+        }
+        
+        System.out.println("Isso é um(a) " + tipo);
+    }
+    
+}
